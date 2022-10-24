a5db122448cb09f8c5bb422948e3e3f3c9db6351
ryanoliveiragit <ryanoliveirasp@gmail.com>
2022-10-24 10:51:23 -0300

    add space

--- src/components/buttons/index.jsx
+++ src/components/buttons/index.jsx
@@ -3,7 +3,7 @@ import Buttons from './styles'
 import { FaWhatsapp } from 'react-icons/fa'
 const Button = ({children}) => {
   return (
-    <Buttons>{children}<FaWhatsapp className='icon' /></Buttons>
+    <Buttons>{children}  <FaWhatsapp className='icon' /></Buttons>
   )
 }
 
